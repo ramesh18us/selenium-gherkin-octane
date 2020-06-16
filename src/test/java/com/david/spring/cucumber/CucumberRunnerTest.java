6e78e8d74556f261bcc1ba9d9963792376cc183d
Ramesh <ramesh.nagarajan@microfocus.com>
2020-06-16 20:00:50 +0000

    First Commit

--- src/test/java/com/david/spring/cucumber/CucumberRunnerTest.java
+++ src/test/java/com/david/spring/cucumber/CucumberRunnerTest.java
@@ -1,11 +1,17 @@
 package com.david.spring.cucumber;
 
+import com.hpe.alm.octane.OctaneCucumber;
 import org.junit.runner.RunWith;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
+@RunWith(OctaneCucumber.class)
+//The below was done to insure a good run no matter what other feature files may be added.
+/*@CucumberOptions(plugin = {"junit:leanftResults.xml"}, features="classpath:advantage.feature",
+		format= {"pretty", "html:target/cucumber-pretty-report"})//the feature to execute
+@RunWith(Cucumber.class)*/
 @CucumberOptions(
 		features = {"src/test/java/com/david/spring/cucumber/features"},
 		plugin = {
